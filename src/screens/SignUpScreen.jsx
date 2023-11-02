5e2ae70771ba2794a6b40f41ca2b9290cad9c59b
harshal <harshrathod1111@gmail.com>
2023-11-02 16:38:47 +0530

    created signup screen

---
+++ src/screens/SignUpScreen.jsx
@@ -0,0 +1,51 @@
+import { useRef } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase";
+import "./SignUpScreen.scss";
+
+const SignUpScreen = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((authUser) => {
+        console.log(authUser.user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="signup">
+      <form>
+        <h1>Sign In</h1>
+        <input ref={emailRef} type="email" placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+        <h4>
+          <span className="signup_gray">New to Netflix? </span>
+          <span className="signup_link" onClick={register}>
+            Sign Up now.
+          </span>
+        </h4>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpScreen;

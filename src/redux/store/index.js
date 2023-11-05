122f29be5cc0527a3a3d31c19b72d9cde1281533
harshal <harshrathod1111@gmail.com>
2023-11-05 12:46:37 +0530

    implemented redux setup

---
+++ src/redux/store/index.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import userReducer from "../userSlice/userSlice";
+
+const store = configureStore(
+  {
+    reducer: {
+      user: userReducer,
+    },
+  },
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
